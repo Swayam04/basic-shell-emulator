7f8c05f13b6de17fad18d64a23058dd9fa49f062
Swayamjeet <swayam.hp001@gmail.com>
2024-12-18 13:28:53 +0530

    Added type support

--- app/main.py
+++ app/main.py
@@ -2,14 +2,20 @@ import sys
 
 
 def main():
-
+    commands = {"exit": 0, "echo": 1, "type": 2}
     while True:
         sys.stdout.write("$ ")
         s = input()
-        if s == "exit 0":
+        if s == "exit":
             break
         elif s.startswith("echo"):
             print(s.lstrip("echo "))
+        elif s.startswith("type"):
+            command_name = s.lstrip("type ")
+            if command_name in commands:
+                print(f"{command_name} is a shell builtin")
+            else:
+                print(f"{command_name}: not found")
         else:
             print(f"{s}: command not found")
 
