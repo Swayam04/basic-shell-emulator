9483caa875a8c60b7d012b9d409680c99661aa1f
Swayamjeet <swayam.hp001@gmail.com>
2024-12-18 13:11:38 +0530

    Added REPL

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
 
-    # Wait for user input
-    s = input()
-    print(f"{s}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        s = input()
+        print(f"{s}: command not found")
 
 
 if __name__ == "__main__":
