5c3653c797aeecfcac4dd2a8b4afa0add525a1fc
Swayamjeet <swayam.hp001@gmail.com>
2024-12-18 13:24:00 +0530

    Added echo support

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ def main():
         s = input()
         if s == "exit 0":
             break
-        print(f"{s}: command not found")
+        elif s.startswith("echo"):
+            print(s.lstrip("echo "))
+        else:
+            print(f"{s}: command not found")
 
 
 if __name__ == "__main__":
